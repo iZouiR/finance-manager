b82dbe2a2040c2002e7867d2b22eab4ad3337ea8
Timofey Kozlov <iZouiR@jotaro.ru>
2024-10-15 13:37:17 +0300

    temporarily remove security from gateway service

--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 }
 
 dependencyManagement {
