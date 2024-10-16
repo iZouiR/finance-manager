985b5d379b379d91a13fe14066963af6192fb400
Timofey Kozlov <iZouiR@jotaro.ru>
2024-10-16 15:59:20 +0300

    add dto package to budget planning service

---
+++ budget-planning-service/src/main/java/org/izouir/budgetplanningservice/dto/ErrorDto.java
@@ -0,0 +1,21 @@
+package org.izouir.budgetplanningservice.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@Schema(description = "Error")
+public class ErrorDto {
+    @Schema(description = "Error code", example = "500")
+    private int code;
+
+    @Schema(description = "Error message", example = "The request parameter {parameterName} is missing...")
+    private String message;
+
+    @Schema(description = "Error timestamp", example = "2021-03-24 16:34:26.666")
+    private Timestamp timestamp;
+}
