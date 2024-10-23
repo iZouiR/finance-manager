396d2b678c6421f43e2a459c5df9f22bc5f285af
Timofey Kozlov <iZouiR@jotaro.ru>
2024-10-23 13:33:10 +0300

    add kafka implementation for transaction service

--- transaction-service/src/main/java/org/izouir/transactionservice/service/impl/TransactionServiceImpl.java
+++ transaction-service/src/main/java/org/izouir/transactionservice/service/impl/TransactionServiceImpl.java
@@ -7,6 +7,8 @@ import org.izouir.transactionservice.mapper.TransactionMapper;
 import org.izouir.transactionservice.repository.AccountRepository;
 import org.izouir.transactionservice.repository.TransactionRepository;
 import org.izouir.transactionservice.service.TransactionService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +16,12 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TransactionServiceImpl implements TransactionService {
+    private final KafkaTemplate<String, TransactionDto> kafkaTemplate;
     private final TransactionRepository transactionRepository;
     private final AccountRepository accountRepository;
+
+    @Value("${spring.kafka.producer.properties.topic-transaction}")
+    private String topicTransaction;
 
     @Override
     public void create(final TransactionDto transactionDto) {
@@ -25,6 +31,7 @@ public class TransactionServiceImpl implements TransactionService {
                         String.format("Account with id = %s not found", accountId)));
         final var transaction = TransactionMapper.toEntity(transactionDto, account);
         transactionRepository.save(transaction);
+        kafkaTemplate.send(topicTransaction, TransactionMapper.toDto(transaction));
     }
 
     @Override
